6b4b7c9cb97abbb80b6579d833c20d7554faf1b2
Jon Loftus <83649313+med1ocre@users.noreply.github.com>
2022-09-21 12:13:06 -0400

    tom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,43 @@
 //Landing page
 //Press spacebar to continue function
-if(inGame == 0){
-  document.body.onkeyup = function(e) {
-    if (e.key == " " ||  e.code == "Space" || e.keyCode == 32) {
+document.body.onkeyup = function(e) {
+  if (e.key == " " || e.code == "Space" || e.keyCode == 32) {
 
-      //Hide the landing text and make the loading icon visible for 1.5 second
-      inGame = 1;
-      document.getElementById("LandingTextid").style.visibility = "hidden";
-      document.getElementById("LandingLoader").style.visibility = "visible";
+    //Hide the landing text and make the loading icon visible for 1.5 second
+    document.getElementById("LandingTextid").style.visibility = "hidden";
+    document.getElementById("LandingLoader").style.visibility = "visible";
 
-      //Get rid of the loading icon after 1.5 second
-      const loadingInterval = setInterval(function () {document.getElementById("LandingLoader").style.visibility = "hidden";}, 1500)
-
-    }
+    //Get rid of the loading icon after 1.5 second
+    const loadingInterval = setInterval(function() {
+      window.location = "main.html";
+    }, 1500)
   }
+}
+
+
+//Function for generating tickets via button
+function GenerateTicket(){
+
+  totalTickets += generateTicketPower;
+
+  generatedTicketBar.setAttribute("value", totalTickets);
+  generatedTicketBar.setAttribute("max", totalTicketCap);
+  generatedTicketBar.setAttribute("data-label", totalTickets + " tickets");
 
 }
+
+//Every second the contents of this function will run
+window.setInterval( function(){
+
+  //Here we check for tickets above the threshold to push to workers
+ if (totalTickets >= ticketThreshold) {
+   //Subtract tickets equal to the ticketthreshold amount and also and them to the queue
+   totalTickets -= ticketThreshold;
+   ticketsInQueue += ticketThreshold;
+   //Subtract a worker from the total workers to start working on tickets
+   inactiveWorkers -= 1;
+
+   document.getElementById("TotalWorkerText").innerHTML = inactiveWorkers + "/" + totalWorkers + " workers";
+
+ }
+},1000)
--- assets/js/variables.js
+++ assets/js/variables.js
@@ -1,2 +1,14 @@
-//Variable for the spacebar continue function . (So that the spacebar only activates once when pressed)
-let inGame = 0;
+//Values for currencies
+let totalTickets = 0;
+let totalTicketCap = 100;
+//Value for the tickets that are waiting to be processed
+let ticketsInQueue = 0;
+//This value determines when the tickets get passed along to the workers
+let ticketThreshold = 10;
+//Values for the workers
+let totalWorkers = 5;
+let inactiveWorkers = 5;
+//Values for clicking powers
+let generateTicketPower = 1;
+//Value to set the ticket progress bar to its HTML element
+let generatedTicketBar = document.getElementById("GeneratedTicketBar");
