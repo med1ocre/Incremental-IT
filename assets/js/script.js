fe829a741b62f0691c65e60fe15b48c4a5a14479
Jon Loftus <83649313+med1ocre@users.noreply.github.com>
2022-11-14 08:29:19 -0500

    sttufff
    dxsdd

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,16 +58,43 @@ formatWithCommas = function(num, decimal) {
 }
 
 window.onload = function(){
+  loadGame();
   UpdateText();
 }
+
+let saveInterval = window.setInterval(function(){
+
+  saveGame();
+
+}, 30000); //30sec
+
+
+let sellInterval = window.setInterval(function(){
+
+  Sellticket(Tickets.SellCount);
+
+}, 2000); //2sec
+
+let ticketInterval = window.setInterval(function(){
+
+  if(Worker.Technician.Amount >= 1){
+    Createticket();
+  }
+
+}, 1000); //1sec
 
 function UpdateText(){
 
   Element.ticketTextDisp.innerHTML = formatWithCommas(Tickets.Total);
   Element.queuedticketsTextDisp.innerHTML = formatWithCommas(Tickets.Queued);
   Element.fundsTextDisp.innerHTML = formatWithCommas(Player.Funds, 2);
+  Element.techniciansTextDisp.innerHTML = formatWithCommas(Worker.Technician.Amount) + " ";
+  Element.techniciancostTextDisp.innerHTML = "($" + formatWithCommas(Worker.Technician.Price, 2) + ")";
+  Element.analystsTextDisp.innerHTML = formatWithCommas(Worker.Analyst.Amount) + " ";
+  Element.analystcostTextDisp.innerHTML = "($" + formatWithCommas(Worker.Analyst.Price, 2) + ")";
 
 }
+
 
 //Function for generating tickets via button
 Element.generateTicketBtn.onclick = function(){
@@ -76,20 +103,49 @@ Element.generateTicketBtn.onclick = function(){
 
   Tickets.Queued += 1;
 
-  UpdateText();
+  Element.ticketTextDisp.innerHTML = formatWithCommas(Tickets.Total);
+  Element.queuedticketsTextDisp.innerHTML = formatWithCommas(Tickets.Queued);
 
 }
 
-function Selltickets(ticketsDemanded){
+
+
+function Createticket(){
+
+  let amount = Worker.Technician.Amount;
+
+  //Create tickets equal to the amount of technicians you have
+
+  Tickets.Total += amount;
+  Tickets.Queued += amount;
+
+  Element.ticketTextDisp.innerHTML = formatWithCommas(Tickets.Total);
+  Element.queuedticketsTextDisp.innerHTML = formatWithCommas(Tickets.Queued);
+
+}
+
+function Sellticket(ticketsDemanded){
 
   if(Tickets.Queued > 0){
 
-    if(ticketsDemanded > Tickets.Queued){
+    if(ticketsDemanded <= Tickets.Queued){
 
-      Tickets.Queued -= ticketsDemanded;
+      Tickets.Queued -= ticketsDemanded + Worker.Analyst.Amount;
+
+      Player.Funds += Tickets.Price * ticketsDemanded + Worker.Analyst.Amount;
 
     }else{
-      
+
+      if(ticketsDemanded > Tickets.Queued){
+
+        transaction = Tickets.Queued;
+
+        Player.Funds += Tickets.Price * transaction;
+
+        Tickets.Queued = 0;
+
+      }
+
     }
 
   }else{
@@ -98,6 +154,100 @@ function Selltickets(ticketsDemanded){
 
   }
 
-  UpdateText();
+  Element.ticketTextDisp.innerHTML = formatWithCommas(Tickets.Total);
+  Element.queuedticketsTextDisp.innerHTML = formatWithCommas(Tickets.Queued);
+  Element.fundsTextDisp.innerHTML = formatWithCommas(Player.Funds, 2);
 
+}
+
+function BuyTechnician(){
+
+  if(Player.Funds >= Worker.Technician.Price){
+
+    Worker.Technician.Amount += 1;
+
+    Player.Funds -= Worker.Technician.Price;
+
+
+
+    //increase cost
+    Worker.Technician.Price = (Math.pow(1.5,Worker.Technician.Amount)+5);
+
+  }else{
+
+    console.log("You don't have enough funds!");
+
+  }
+
+  Element.techniciansTextDisp.innerHTML = formatWithCommas(Worker.Technician.Amount) + " ";
+  Element.techniciancostTextDisp.innerHTML = "($" + formatWithCommas(Worker.Technician.Price, 2) + ")";
+
+
+}
+
+function BuyAnalyst(){
+
+  if(Player.Funds >= Worker.Analyst.Price){
+
+    Worker.Analyst.Amount += 1;
+
+    Player.Funds -= Worker.Analyst.Price;
+
+
+    //increase cost
+    Worker.Analyst.Price = (Math.pow(1.5,Worker.Analyst.Amount)+5);
+
+  }else{
+
+    console.log("You don't have enough funds!");
+
+  }
+
+  Element.analystsTextDisp.innerHTML = formatWithCommas(Worker.Analyst.Amount) + " ";
+  Element.analystcostTextDisp.innerHTML = "($" + formatWithCommas(Worker.Analyst.Price, 2) + ")";
+
+}
+
+function DisplayMessage(msg){
+
+  Element.readout5TextDisp.innerHTML = Element.readout4TextDisp.innerHTML;
+  Element.readout4TextDisp.innerHTML = Element.readout3TextDisp.innerHTML;
+  Element.readout3TextDisp.innerHTML = Element.readout2TextDisp.innerHTML;
+  Element.readout2TextDisp.innerHTML = Element.readout1TextDisp.innerHTML;
+  Element.readout1TextDisp.innerHTML = msg;
+
+
+}
+
+
+function loadGame(){
+  //unparse the save
+  var saveGame = JSON.parse(localStorage.getItem("gameSave"));
+  //Make sure that the variable is actually defined, this is for updates so we dont throw any errors
+  if(typeof saveGame.Player !== "undefined"){
+    Player = saveGame.Player;
+  }
+  if(typeof saveGame.Tickets !== "undefined"){
+    Tickets = saveGame.Tickets;
+  }
+  if(typeof saveGame.Worker !== "undefined"){
+    Worker = saveGame.Worker;
+  }
+}
+
+//function to save our game
+function saveGame(){
+  var gameSave = {
+    //setting our obects/values
+    Player: Player,
+    Tickets: Tickets,
+    Worker: Worker
+  };
+  //stringify it for readability
+  localStorage.setItem("gameSave", JSON.stringify(gameSave));
+}
+
+function reset() {
+    localStorage.removeItem("gameSave");
+    location.reload();
 }
--- assets/js/variables.js
+++ assets/js/variables.js
@@ -2,7 +2,23 @@
 let Tickets = {
   Total: 0,
   Queued: 0,
-  Price: 1
+  SellCount: 1,
+  Price: 0.25,
+  PerSec: 0
+}
+
+let Worker = {
+
+  Technician: {
+    Amount: 0,
+    Price: 5
+  },
+  Analyst: {
+    Amount: 0,
+    Price: 5
+  }
+
+
 }
 
 let Player = {
@@ -14,5 +30,16 @@ let Element = {
    landingTextDisp: document.getElementById("LandingTextid"),
    ticketTextDisp: document.getElementById("tix"),
    fundsTextDisp: document.getElementById("funds"),
-   queuedticketsTextDisp: document.getElementById("queuedtickets")
+   queuedticketsTextDisp: document.getElementById("queuedtickets"),
+   techniciansTextDisp: document.getElementById("technicians"),
+   techniciancostTextDisp: document.getElementById("techniciancost"),
+   analystsTextDisp: document.getElementById("analysts"),
+   analystcostTextDisp: document.getElementById("analystcost"),
+
+   readout1TextDisp: document.getElementById("readout1"),
+   readout2TextDisp: document.getElementById("readout2"),
+   readout3TextDisp: document.getElementById("readout3"),
+   readout4TextDisp: document.getElementById("readout4"),
+   readout5TextDisp: document.getElementById("readout5"),
+
 }
